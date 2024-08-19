bad73835256e52bc39469a35cab7b4f9cac90916
Yousef-Sayed-Younis <yousef.sayed.younis@gmail.com>
2024-08-20 00:07:36 +0200

    Adding README file

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,3 @@ void main() async {
 
   runApp(const NoteApp());
 }
-
-/*
-  * To Learn:
-    - Fix Imports
-    - Fix Spelling
-    - App Architecture
-    - Split logic from UI
-    - Notice unused variable
-    - Use ThemeMode instead of ThemeData
-    - Take advantages of Theme as possible
-    - Naming of properties: lowerCamelCase
-    - Clean main.dart without any additions
-    - Learn difference between Screen, View, Page
-    - Notice unused parameters and replace with '_'
-    - Use theme for theming Widgets like 'TextField'
-    - Naming of Methods: starts with verb, lowerCamelCase
-    - Don't leave variables or methods in file without class
-    - Use 'const' keyword if possible, for widgets and constructors
-    - Remove last comma ',', use expression body methods and format code
-*/
